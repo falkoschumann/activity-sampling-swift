b3a7e18a681eb81f1367b2ffbad2f6aef66a12b4
Falko Schumann <falko.schumann@muspellheim.de>
2021-12-12 16:31:18 +0100

    Lege Projekt an

---
+++ ActivitySampling/ActivitySamplingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivitySamplingApp.swift
+//  ActivitySampling
+//
+//  Created by Falko Schumann on 12.12.21.
+//
+
+import SwiftUI
+
+@main
+struct ActivitySamplingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
